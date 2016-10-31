0675ed2b8d3d554472bd6f00c6799cb5e6595f61
Özgür TÜZGEN <ozgur.tuzgen@arge.local>
2016-10-31 11:52:18 +0300

    test setup and a simple test

---
+++ app/city-list.component.spec.ts
@@ -0,0 +1,10 @@
+// import {TestBed} from '@angular/core/testing';
+
+// import {By} from '@angular/platform-browser';
+
+
+describe('Initial Tests', ()=>{
+    it('should be true', ()=>{
+        expect(true).toEqual(true);
+    });
+});
--- systemjs.config.js
+++ systemjs.config.js
@@ -1,4 +1,3 @@
-
 /**
  * System configuration for Angular samples
  * Adjust as necessary for your application needs.
@@ -13,6 +12,7 @@
         map: {
             // our app is within the app folder
             app: 'app',
+
             // angular bundles
             '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
             '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
@@ -22,6 +22,7 @@
             '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
             '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+            
             // other libraries
             'rxjs':                      'npm:rxjs',
             'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
