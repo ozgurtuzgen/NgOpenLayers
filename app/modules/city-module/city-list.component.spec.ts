7ae189547f9286468aaf2ee2dd4570831177df8b
Özgür TÜZGEN <ozgur.tuzgen@arge.local>
2016-11-01 18:02:07 +0300

    async service call test

--- app/modules/city-module/city-list.component.spec.ts
+++ app/modules/city-module/city-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { DebugElement } from '@angular/core';
 
 import { STMCityListComponent } from './city-list.component';
@@ -11,21 +11,36 @@ import { By } from '@angular/platform-browser';
 
 let comp: STMCityListComponent;
 let fixture: ComponentFixture<STMCityListComponent>;
-let de: DebugElement; // debug edilen element
-let el: HTMLElement; // karsilastirilacak gercek element
+let deTitle: DebugElement; // debug edilen element
+let deCityList: DebugElement; // debug edilen element
+let elTitle: HTMLElement; // karsilastirilacak gercek element
+let elCityList: HTMLElement; // karsilastirilacak gercek element
+
+let cityService: CityService;
+
+let spy: jasmine.Spy;
+
 
 beforeEach(() => {
 
     TestBed.configureTestingModule({
-        declarations: [STMCityListComponent]
+        declarations: [STMCityListComponent],
+        providers: [CityService]
     });
 
     fixture = TestBed.createComponent(STMCityListComponent);
     comp = fixture.componentInstance;
 
+    //service injection
+    cityService = fixture.debugElement.injector.get(CityService);
 
-    de = fixture.debugElement.query(By.css('h2'));
-    el = de.nativeElement;
+    spy = spyOn(cityService, 'getCityList').and.returnValue(Promise.resolve(CityList));
+
+    deCityList = fixture.debugElement.query(By.css('.illerClass'));
+    elCityList = deCityList.nativeElement;
+
+    deTitle = fixture.debugElement.query(By.css('h2'));
+    elTitle = deTitle.nativeElement;
 });
 
 describe('City list component', () => {
@@ -35,22 +50,41 @@ describe('City list component', () => {
 
     it('should contain list title as İller', () => {
         fixture.detectChanges();
-        const content = el.textContent;
+        const content = elTitle.textContent;
         expect(content).toContain("İller");
     });
 
     it('should display original title', () => {
         fixture.detectChanges();
-        expect(el.textContent).toContain(comp.title);
+        expect(elTitle.textContent).toContain(comp.title);
     });
 
     it('should display a different test title', () => {
         comp.title = 'Test Title';
         fixture.detectChanges();
-        expect(el.textContent).toContain('Test Title');
+        expect(elTitle.textContent).toContain('Test Title');
     });
 
     it('no title in the DOM until manually call `detectChanges`', () => {
-        expect(el.textContent).toEqual('');
+        expect(elTitle.textContent).toEqual('');
+    });   
+
+    it('should not show city list before OnInit', () => {        
+        expect(spy.calls.any()).toBe(false, 'getQuote not yet called');
     });
+
+    it('should still not show city list after component initialized', () => {
+        fixture.detectChanges();
+        // getQuote service is async => still has not returned with city list        
+        expect(spy.calls.any()).toBe(true, 'getQuote called');
+    });
+
+    it('should show city list after getCityList promise (async)', async(() => {
+        fixture.detectChanges();
+
+        fixture.whenStable().then(() => { // wait for async getQuote
+            fixture.detectChanges();        // update view with quote
+            expect(elCityList.children.length).toBe(3);
+        });
+    }));
 });
