65d66f943ffa1dd1426bbb6cae2b9bfccacf1d1f
kadirteke <kadirteke@gmail.com>
2016-11-07 22:19:27 +0300

    app.component.html eklendi

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,16 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-       <nav>
-         <a routerLink="basic" routerLinkActive="active"><h1>Basic</h1></a>
-         <a routerLink="city"  routerLinkActive="active"><h1>Cities With Map</h1></a>
-         <a routerLink="ui-deneme"  routerLinkActive="active"><h1>UI Library Denemeleri</h1></a>
-         <a routerLink="rest"  routerLinkActive="active"><h1>Rest Denemeleri</h1></a>
-       </nav>
-       <h1>{{title}}</h1>
-      <router-outlet></router-outlet>
-  `,
+  templateUrl: "app/app.component.html",
 })
 export class AppComponent {
   title = 'Root Module, modulden modüle değişmeyen kısımlar. Ana layout gibi.';
