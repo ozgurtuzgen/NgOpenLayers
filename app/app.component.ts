af756bd6b93cdb0c239daea1a87a8b8b52b09500
Şeyda KÜÇÜK <shatipoglu@stm.com.tr>
2016-12-12 16:47:24 +0300

    acilista map componentin gelmesi saglandi

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,11 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
        <nav>
-         <a routerLink="basic" routerLinkActive="active"><h1>Basic</h1></a>
-         <a routerLink="city"  routerLinkActive="active"><h1>Cities With Map</h1></a>
-         <a routerLink="ui-deneme"  routerLinkActive="active"><h1>UI Library Denemeleri</h1></a>
+    <!--    <a routerLink="basic" routerLinkActive="active"><h1>Basic</h1></a>-->
+         <a routerLink="city"  routerLinkActive="active"><h1>MEYTAP Harita Uygulaması</h1></a>
+    <!--  <a routerLink="ui-deneme"  routerLinkActive="active"><h1>UI Library Denemeleri</h1></a>-->
        </nav>
-       <h1>{{title}}</h1>
+      <!-- <h1>{{title}}</h1>-->
       <router-outlet></router-outlet>
   `,
 })
--- app/routing.module.ts
+++ app/routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule }             from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'basic', pathMatch: 'full'},
-  { path: 'city', loadChildren: 'app/modules/city-module/city.module#CityModule' },
+  { path: 'basic', redirectTo: 'basic', pathMatch: 'full'},
+  { path: '', loadChildren: 'app/modules/city-module/city.module#CityModule' },
   { path: 'ui-deneme', loadChildren: 'app/modules/ui-deneme-module/ui.module#UiModule' }
 ];
 
